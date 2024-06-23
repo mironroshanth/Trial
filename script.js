9a3c8d47f6e7576adb5228c5d8dbd9c36624ec95
Daniel Miron <165149279+mironroshanth@users.noreply.github.com>
2024-06-23 16:11:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,12 @@ function playSound(note) {
   audio.src = `audio/D.mp3`;
   audio.src = `audio/Eb.mp3`;
   audio.src = `audio/E.mp3`;
+  audio.src = `audio/F.mp3`;
+  audio.src = `audio/Gb.mp3`;
+  audio.src = `audio/G.mp3`;
+  audio.src = `audio/Ab.mp3`;
+  audio.src = `audio/A.mp3`;
+  audio.src = `audio/Bb.mp3`;
+  audio.src = `audio/B.mp3`;
   audio.play();
 }
