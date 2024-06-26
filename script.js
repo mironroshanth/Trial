57bcd616a33c9289d62c7ea34703b4e1ca97403c
Daniel Miron <165149279+mironroshanth@users.noreply.github.com>
2024-06-26 17:42:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function playSound(note) {
-  const audio = new Audio();
-  audio.src = `audio/C.mp3`;
-  audio.src = `audio/Db.mp3`;
-  audio.src = `audio/D.mp3`;
-  audio.src = `audio/Eb.mp3`;
-  audio.src = `audio/E.mp3`;
-  audio.src = `audio/F.mp3`;
-  audio.src = `audio/Gb.mp3`;
-  audio.src = `audio/G.mp3`;
-  audio.src = `audio/Ab.mp3`;
-  audio.src = `audio/A.mp3`;
-  audio.src = `audio/Bb.mp3`;
-  audio.src = `audio/B.mp3`;
-  audio.play();
+    var audio = document.getElementById('audio-C');
+    var audio = document.getElementById('audio-Db');
+    var audio = document.getElementById('audio-D');
+    var audio = document.getElementById('audio-Eb');
+    var audio = document.getElementById('audio-E');
+    var audio = document.getElementById('audio-F');
+    var audio = document.getElementById('audio-Gb');
+    var audio = document.getElementById('audio-G');
+    var audio = document.getElementById('audio-Ab');
+    var audio = document.getElementById('audio-A');
+    var audio = document.getElementById('audio-Bb');
+    var audio = document.getElementById('audio-B');
+
+    if (audio) {
+        audio.currentTime = 0; // Rewind to the beginning
+        audio.play();
+    }
 }
